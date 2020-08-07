b6653eebcc8c8690e1e5baf53aa3c2d436a8db1a
Black V <BlackV@users.noreply.github.com>
2020-08-08 09:46:43 +1200

    Update Copoy-VMWareTools.ps1
    Formatting

--- Copoy-VMWareTools.ps1
+++ Copoy-VMWareTools.ps1
@@ -6,19 +6,19 @@
 .EXAMPLE
     Example of how to use this cmdlet
 .EXAMPLE
-    Another example of how to use this cmdlet
+    Another example of how to use this cmdlet.
 #>
 function Copy-VMWareTools
 {
     [CmdletBinding()]
-Param
+    Param
     (
         # Path to save the downloaded file (defaults to TEMP)
         [string]
-        $Path=$env:TEMP ,
+        $Path = $env:TEMP ,
 
         # Param2 help description
-        [ValidateSet('AutoDetect','x64','x86','Both')]
+        [ValidateSet('AutoDetect', 'x64', 'x86', 'Both')]
         [string]
         $CPUType
     )
@@ -26,38 +26,49 @@ Param
     Begin
     {
 
-    function DoToolsExist ($TestPath)
-    {
-        if (Test-Path $TestPath)
+        function DoToolsExist ($TestPath)
+        {
+            if (Test-Path $TestPath)
             {
-            Write-Warning -WarningAction Inquire "the file $TestPath already exists"
+                Write-Warning -WarningAction Inquire "the file $TestPath already exists"
             }
-    }
+        }
 
-    $VMWAreurl = 'https://packages.vmware.com/tools/releases'
-    $latest = (Invoke-WebRequest -Uri $VMWAreurl).links |select innerhtml  |select -Last 2 |select -First 1 -ExpandProperty innerhtml
-    $Vmwarex64tool = (Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/x64").links.href | Where-Object {$_ -like '*.exe'}
-    $Vmwarex86tool = (Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/x86").links.href | Where-Object {$_ -like '*.exe'}
+        $VMWAreurl = 'https://packages.vmware.com/tools/releases'
+        $latest = (Invoke-WebRequest -Uri $VMWAreurl).links | select innerhtml  | select -Last 2 | select -First 1 -ExpandProperty innerhtml
+        $Vmwarex64tool = (Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/x64").links.href | Where-Object { $_ -like '*.exe' }
+        $Vmwarex86tool = (Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/x86").links.href | Where-Object { $_ -like '*.exe' }
     }
     Process
     {
-    switch ($CPUType)
+        switch ($CPUType)
         {
-        'AutoDetect' {if ([Environment]::Is64BitOperatingSystem){
-                            DoToolsExist -TestPath "$path\$(($Vmwarex64tool).Split('/')[1])"
-                            Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/$Vmwarex64tool" -OutFile "$path\$(($Vmwarex64tool).Split('/')[1])"
-                            }
-                        else {
-                            Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/$Vmwarex86tool" -OutFile "$path\$(($Vmwarex86tool).Split('/')[1])"
-                            }
-                        }
-        'x64'         {Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/$Vmwarex64tool" -OutFile "$path\$(($Vmwarex64tool).Split('/')[1])"}
-        'x86'         {Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/$Vmwarex86tool" -OutFile "$path\$(($Vmwarex86tool).Split('/')[1])"}
-        'Both'        {
-                        Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/$Vmwarex86tool" -OutFile "$path\$(($Vmwarex86tool).Split('/')[1])"
-                        Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/$Vmwarex64tool" -OutFile "$path\$(($Vmwarex64tool).Split('/')[1])"
-                        }
+            'AutoDetect'
+            {
+                if ([Environment]::Is64BitOperatingSystem)
+                {
+                    DoToolsExist -TestPath "$path\$(($Vmwarex64tool).Split('/')[1])"
+                    Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/$Vmwarex64tool" -OutFile "$path\$(($Vmwarex64tool).Split('/')[1])"
+                }
+                else
+                {
+                    Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/$Vmwarex86tool" -OutFile "$path\$(($Vmwarex86tool).Split('/')[1])"
+                }
             }
+            'x64'
+            {
+                Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/$Vmwarex64tool" -OutFile "$path\$(($Vmwarex64tool).Split('/')[1])"
+            }
+            'x86'
+            {
+                Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/$Vmwarex86tool" -OutFile "$path\$(($Vmwarex86tool).Split('/')[1])"
+            }
+            'Both'
+            {
+                Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/$Vmwarex86tool" -OutFile "$path\$(($Vmwarex86tool).Split('/')[1])"
+                Invoke-WebRequest -Uri "$VMWAreurl/$latest/windows/$Vmwarex64tool" -OutFile "$path\$(($Vmwarex64tool).Split('/')[1])"
+            }
+        }
     }
     End
     {
